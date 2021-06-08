5518e32405df4a8de1b413afae9eae2ebc062e76
Darth9 <reyprimerjefe@gmail.com>
2021-06-08 16:44:58 +0100

    Allow busybox to compile without editing anything in external/busybox

--- Android.mk
+++ Android.mk
@@ -435,7 +435,7 @@ LOCAL_MODULE := recovery
 RECOVERY_API_VERSION := 3
 RECOVERY_FSTAB_VERSION := 2
 LOCAL_CFLAGS += -DRECOVERY_API_VERSION=$(RECOVERY_API_VERSION)
-LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-function -Wno-format-extra-args
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-function -Wno-format-extra-args -Wno-unused-result
 LOCAL_CLANG := true
 ifeq ($(TARGET_ARCH), arm)
   LOCAL_CFLAGS += -Wno-format
